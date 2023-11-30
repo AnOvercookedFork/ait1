257476494b037afb775b3a3fdb59e8eef9c670e7
RadiantRaccoon <oldamazingbrain@gmail.com>
2023-11-29 22:36:01 -0500

    working-ish implementation of dqn

--- dqn.py
+++ dqn.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import gymnasium as gym
 import random
-
+import tqdm
 class ReplayBuffer:
     def __init__(self, capacity: int):
         self.capacity = capacity
@@ -23,54 +23,85 @@ class DQN:
     def __init__(self, env: gym.Env):
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Dense(64, activation='relu', input_dim=env.observation_space.shape[0]),
-            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(32, activation='relu'),
             tf.keras.layers.Dense(env.action_space.n, activation='linear')
         ])
         self.model.compile(optimizer='adam', loss='mse')
 
-        self.buffer = ReplayBuffer(100000)
+        self.buffer = ReplayBuffer(50000)
 
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.gamma = 0.95
-        self.samples_per_train = 10
-        self.batch_size = 1000
+        self.gamma = 0.995
+        self.samples_per_train = 1
+        self.batch_size = 5000
+        self.steps = 0
+        self.step_penalty = 0
 
         self.env = env
         self.state = None
         self.done = False
         self.reset_env()
     
+    def replace_env(self, env): # ONLY WORKS WITH SAME ENV TYPE
+        self.env = env
+        self.steps = 0
+        self.reset_env()
+
     def decay_epsilon(self):
         self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
 
     def reset_env(self):
-        self.state = self.env.reset()
+        self.state, _ = self.env.reset()
+        self.done = False
 
-    def run_step(self):
-        action = np.argmax(self.model.predict(self.state)) if np.random.rand() > self.epsilon else self.env.action_space.sample()
-        next_state, reward, terminated, truncated, _, _ = self.env.step(action)
-        self.buffer.add((self.state, action, reward, next_state, terminated or truncated))
+    def run_step(self, inference = False):
+        # print(f'state: {self.state}')
+        action = None
+        if inference or np.random.rand() > self.epsilon:
+            # print('predicting...')
+            # print(f'state shape: {self.state.shape}')
+            preds = self.model.predict(np.reshape(self.state, (1, -1)), verbose=0)[0]
+            action = np.argmax(preds)
+            if inference:
+                print(f'Max Q-val: {np.amax(preds)}')
+        else:
+            action = self.env.action_space.sample()
+        # action = np.argmax(self.model.predict(self.state)[0]) if inference or np.random.rand() > self.epsilon else self.env.action_space.sample()
+        next_state, reward, terminated, truncated, _ = self.env.step(action)
+        self.buffer.add((self.state, action, reward, next_state, terminated or truncated, self.steps))
         self.state = next_state
         self.done = terminated or truncated
 
-    def run_iteration(self):
-        while not self.done:
-            self.run_step()
+    def run_iteration(self, inference = False):
+        for _ in range(0, 5000):
+            self.steps += 1
+            if not self.done:
+                self.run_step(inference)
+        self.reset_env()
     
     def train_network(self):
+        # print('training...')
         minibatch = self.buffer.sample(self.batch_size)
-        for state, action, reward, next, done in minibatch:
-            target = reward
+        states = np.array([np.reshape(state, (1, -1))[0] for state, _, _, _, _, _ in minibatch])
+        next_states = np.array([np.reshape(next_state, (1, -1))[0] for _, _, _, next_state, _, _ in minibatch])
+        steps = [a for _, _, _, _, _, a in minibatch]
+        q_values = self.model.predict(states, verbose=0)
+        next_q_values = self.model.predict(next_states, verbose=0)
+        targets = q_values.copy()
+        for i, (_, action, reward, _, done, _) in enumerate(minibatch):
+            target = reward #- steps[i] * self.step_penalty
             if not done:
-                target += self.gamma * np.amax(self.model.predict(next)[0])
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+                target += self.gamma * np.amax(next_q_values[i])
+            targets[i, action] = target
+        self.model.fit(states, targets, epochs=5, verbose=0)
 
     def train(self, iterations = 1000):
-        for i in range(1, iterations + 1):
+        for i in tqdm.tqdm(range(0, iterations)):
+            # print(i)
             if self.buffer.size() >= self.batch_size: # nested for branch prediction efficiency
-                if i % self.samples_per_train == 0:
+                if (i + 1) % self.samples_per_train == 0:
                     self.train_network()
+            self.run_iteration()
+            self.decay_epsilon()
--- hellogym.py
+++ hellogym.py
@@ -1,17 +1,53 @@
 import gymnasium as gym
+import dqn
+import sys
 
 def main():
-    env = gym.make("LunarLander-v2", render_mode="human")
-    print("Action space:", env.action_space)
-    print("Observation space:", env.observation_space)
-    observation, info = env.reset(seed=42)
-    for _ in range(1000):
-        action = env.action_space.sample()  # this is where you would insert your policy
-        observation, reward, terminated, truncated, info = env.step(action)
-        if terminated or truncated:
-            observation, info = env.reset()
+    do_stuff()
+    # env = gym.make("LunarLander-v2", render_mode="human")
+    # train_env = gym.make('LunarLander-v2')
+    # agent = dqn.DQN(train_env)
+    # agent.train(200)
+    # agent.replace_env(env)
+    # agent.run_iteration(True)
+    # train_env.close()
+    # print("Action space:", env.action_space)
+    # print("Observation space:", env.observation_space)
+    # observation, info = env.reset(seed=42)
+    # for _ in range(1000):
+    #     action = env.action_space.sample()  # this is where you would insert your policy
+    #     observation, reward, terminated, truncated, info = env.step(action)
+    #     if terminated or truncated:
+    #         observation, info = env.reset()
 
-    env.close()
+    # env.close()
+
+def do_stuff():
+    env = gym.make("LunarLander-v2", render_mode="human")
+    train_env = gym.make('LunarLander-v2')
+    agent = dqn.DQN(train_env)
+    while True:
+        try:
+            s = input('Command?')
+            try:
+                match s:
+                    case 'exit':
+                        print('exitting')
+                        sys.exit(0)
+                    case 'train':
+                        num = int(input('Train iterations?'))
+                        agent.replace_env(train_env)
+                        agent.train(num)
+                    case 'play':
+                        agent.replace_env(env)
+                        agent.run_iteration(True)
+            except KeyboardInterrupt:
+                print('Round interrupted')
+                agent.reset_env()
+        except KeyboardInterrupt:
+            print('Interrupted')
+            sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
